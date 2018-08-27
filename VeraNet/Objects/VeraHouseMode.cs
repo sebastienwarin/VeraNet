69bfaebcfe9d3e1a6c550d8e1bf770ffa619ab06
Sébastien Warin <sebastien@warin.fr>
2018-08-27 09:55:39 +0200

    Adding the House Mode (Get & Set)

---
+++ VeraNet/Objects/VeraHouseMode.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="VeraHouseMode.cs" company="Sebastien.warin.Fr">
+//  Copyright 2018 - Sebastien.warin.fr
+// </copyright>
+// <author>Sebastien Warin</author>
+// -----------------------------------------------------------------------
+
+namespace VeraNet.Objects
+{
+    /// <summary>
+    /// Represent the House's mode
+    /// </summary>
+    public enum VeraHouseMode
+    {
+        None = 0,
+        Home = 1,
+        Away = 2,
+        Night = 3,
+        Vacation = 4
+    }
+}
